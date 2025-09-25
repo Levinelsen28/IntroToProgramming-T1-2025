8a9e981eb54974124c542e9b3aca8cb789336a2d
Nelsen Levi <834075@stma.k12.mn.us>
2025-09-25 14:04:59 -0500

    Initial Commit

--- 2.3-practice.py
+++ 2.3-practice.py
@@ -9,6 +9,9 @@ snake_case = True
 screaming_snake_case = False
 
 print(x * y * potato)
-
 ("x > 10")
 print("x = 11")
+num = input("What number do you want to square?\n> ")
+#Parse (convert) the string to an integer
+num = int(num)
+print(num * num)
---
+++ Assignments/SpaceShuttleSupply.py
@@ -0,0 +1,5 @@
+countdown = input("How long until launch?\n> ")
+oxygen_tanks = input("Hwo many oxygentanks do we have?\n> ")
+food_packs = input("How many foodpacks do we have?\n> ")
+water_packs = input("How many waterpacks do we have?\n> ")
+
