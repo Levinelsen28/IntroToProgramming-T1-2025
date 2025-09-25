822c339fa8f78d3bb5b75595b231be05921c0735
Nelsen Levi <834075@stma.k12.mn.us>
2025-09-25 13:15:13 -0500

    Inital Commit

---
+++ Practice/2.3-practice.py
@@ -0,0 +1,14 @@
+x = 10
+y = 50
+potato = 7
+
+lowerCase = True
+upperCase = True
+cammelCase = True
+snake_case = True 
+screaming_snake_case = False
+
+print(x * y * potato)
+
+("x > 10")
+print("x = 11")
