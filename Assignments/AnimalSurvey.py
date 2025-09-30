7feaea7af75c20ba346be2c362fbe1cbbf862b52
Nelsen Levi <834075@stma.k12.mn.us>
2025-09-30 13:30:26 -0500

    initial commit

--- Assignments/AnimalSurvey.py
+++ Assignments/AnimalSurvey.py
@@ -1,14 +1,25 @@
 q1 = input("What is your favorite animal?\n>")
+# this is your favorite animal question
 q2 = input("Do they fly, walk, or swim?\n>")
+# this is asking about how they move
 q3 = input("What sound does the animal make?\n>")
+# this is asking what noise does the animal make
 q4 = input("Where do they live?\n>")
+# this is asking where they live at
 q5 = input("Why do you like them?\n>")
+# this is asking about whay you like them
 q6 = input("Why is this your favorite animal?\n>")
+#this is asking why they are tour favorite
 q7 = input("Do they have scales, fur, feathers, or do they feel slimy?\n>")
+# this is asking what the fell like
 q8 = input("what color are they?\n>")
+# this is asking what color does the animal scales fur feathers are
 q9 = input("What is a fun fact about them?\n>")
+# is just to know what they are about 
 q10 = input("Is it a pet or a wild animal?\n>")
+
 q11 = input("How big is the animal?\n>")
+
 q12 = input("How big is the animal?\n>")
 
-summary = print("So, your favorite animal is" + q1 + " " + q2  + "!")
+summary = f"So, your favorite animal is" + q1 + " " + q2  + "!"
---
+++ Assignments/Mad-Lib.py
@@ -0,0 +1,38 @@
+'''
+Once upon a time a [noun], in a [adjective] [noun],
+had lived in a [adjective] [noun].
+Every day, the [noun] would [verb] [adverb].
+One day, a [adjective] [noun] appeared
+and [verb] the [noun] away.
+The [noun] was [adjective] and [emotion].
+'''
+'''
+Pseudocode
+input 7 nouns
+input 3 adjective
+input 2 verbs
+input 1 adverb
+input 1 emotion
+'''
+print("Give me 7 Nouns")
+noun_1 = input("Noun1\n>")
+noun_2 =input("Noun2\n>")
+noun_3 = input("Noun3\n>")
+noun_4 =input("Noun4\n>")
+noun_5 = input("Noun5\n>")
+noun_6 = input("Noun6\n>")
+noun_7 = input("Noun7\n>")
+print("Give me 3 Adj")
+adj_1 =input("Adj1\n>")
+adj_2 =input(" Adj2\n>")
+adj_3 =input("Adj3\n>")
+print("Give me 2 verb")
+verb_1 = input("verb1\n>")
+verb_2 = input("verb2\n>")
+print("Give me 1 adverb")
+adverb_1 = input("adverb 1\n>")
+print("Give me 1 emotion")
+emotion_1 = input("emotion1\n>")
+
+
+print("Once upon a time a {noun_1}, in a {adj_1} {noun_2}, had lived in a {adj_2} {noun_3}. Every day, the {noun_4} would {verb_1} {adverb_1}. One day, a {adj_3} {noun_5} appeared and {verb_2} the {noun_6} away. The {noun_7} was {adj_4} and {emotion_1}.")king
