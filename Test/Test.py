308f0b5614bc6a41885bd974307b2953053ae3d3
Nelsen Levi <834075@stma.k12.mn.us>
2025-10-06 14:00:50 -0500

    intial commit

---
+++ Test/Test.py
@@ -0,0 +1,21 @@
+#1 
+word_1 = input("Give me a word.\n>")
+word_2 = input("Give me a word.\n>")
+word_3 = input("Give me a word.\n>")
+
+print("So, the word you gave me are " + word_1 + "," + word_2 + ", and " + word_3 +". Thank you" )
+
+#2
+def add_three(x,y,z):
+    input(int(x) + int(y) + int(z))
+
+add_three(10, 15, 20)
+add_three(50, 30, 20)
+add_three(1, 60, 9)
+
+#3
+
+def data_three(int, float):
+    input(int() + float()) 
+
+word = add_three(6+5)
